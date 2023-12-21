906143007e2c5926b4adba7a7a1cc9afd2512dc6
Peerawat <peerawat.jai@ku.th>
2023-12-21 22:40:40 +0700

    first commit

---
+++ mywabsite/admin/admin_form_add.php
@@ -0,0 +1,79 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>ฟอร์มเพิ่มข้อมูล Admin</h1>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card card-outline card-info">
+            <div class="card-body">
+              <div class="card card-primary">
+                <!-- form start -->
+                <form>
+                  <div class="card-body">
+
+                    <div class="form-group row ">
+                      <label class="col-sm-2" >Username</label>
+                      <div class="col-sm-4" >
+                            <input type="text" class="form-control" required placeholder="Username">
+                      </div>
+                    </div>
+                  
+
+                  <div class="form-group row ">
+                      <label class="col-sm-2" >Password</label>
+                      <div class="col-sm-4" >
+                            <input type="Password" class="form-control" required placeholder="Password">
+                      </div>
+                    </div>
+
+                    <div class="form-group row ">
+                      <label class="col-sm-2" >ชื่อผู้ดูแลระบบ</label>
+                      <div class="col-sm-4" >
+                            <input type="text" class="form-control" required placeholder="ชื่อผู้ดูแลระบบ">
+                      </div>
+                    </div>
+
+                    <!-- <div class="form-group row ">
+                      <label class="col-sm-2" >ภาพโปรไฟล์</label>
+                      <div class="col-sm-4" >
+                            <input type="file" class="form-control" required accept="image/*" >
+                      </div>
+                    </div> -->
+
+                    <div class="form-group row ">
+                      <label class="col-sm-2" ></label>
+                      <div class="col-sm-4" >
+                      <!-- <button type="submit" class="btn btn-primary btn-lg btn-block">เพิ่มข้อมูล</button> -->
+
+                      <button type="submit" class="btn btn-primary">เพิ่มข้อมูล</button>
+                      <a href="" class="btn btn-danger">ยกเลิก</a>
+                      </div>
+                    </div>
+                
+                 </div><!-- /.card-body -->
+
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.col-->
+      </div>
+      <!-- ./row -->
+
+      <!-- ./row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
---
+++ mywabsite/login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Log in</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="assets/dist/css/adminlte.min.css?v=3.2.0">
+</head>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <b>K U</b> Ems
+        </div>
+
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Login เข้าใช้งานระบบ</p>
+                <form action="assets//index3.html" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Username">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-success btn-block">เข้าสู่ระบบ</button>
+                        </div>
+
+                    </div>
+                </form>
+                <div class="social-auth-links text-center mb-3">
+                    <p>- OR -</p>
+                    <a href="#" class="btn btn-block btn-light">
+                        <i class="fab  fa-google mr-2"></i> Google
+                    </a>
+                </div>
+
+                <p class="mb-1">
+                    <a href="forgot-password.html">ลืมรหัสผ่าน</a>
+                </p>
+                <p class="mb-0">
+                    <a href="register.html" class="text-center">สมัครเข้าใช้งาน</a>
+                </p>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
